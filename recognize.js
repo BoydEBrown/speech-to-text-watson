d703a4cd8c367e5a44fcf310c8150bfc8f4e7215
sbpolicar <sbpolicar@gmail.com>
2016-09-22 15:07:34 -0700

    limit

--- recognize.js
+++ recognize.js
@@ -9,6 +9,7 @@ var speech_to_text = new SpeechToTextV1({
 
 var checked = [],
     files = fs.readdirSync('./resources'),
+    currentItem = '',
     ready = false;
 
 files.forEach(function(file, idx){
@@ -18,28 +19,43 @@ files.forEach(function(file, idx){
       ready = true;
     }
   } else {
-    if(file[0] != '.'){
-      console.log("Bad file type, format must be .WAV");
-      if(idx === files.length - 1) {
-        ready = true;
-      }
+    console.log("Bad file type, format must be .WAV");
+    if(idx === files.length - 1) {
+      ready = true;
     }
   }
 });
 
 if(ready){
-  async.eachSeries(checked, function(item, callback) {
-    console.log("Running "+item);
-    fs.createReadStream('./resources/'+item)
-    .pipe(
-      speech_to_text.createRecognizeStream({
-        content_type: 'audio/l16; rate=44100'
-      })
-    ).pipe(
-      fs.createWriteStream('./results/'+item+'.txt')
-    ).on('finish', function(){
-      console.log("Conversion finished for "+item);
-      callback(null, true);
+  fs.writeFile('./resources/tracker/tracker.txt', checked);
+  try {
+    async.eachLimit(checked, process.env.FILE_ASYNC_LIMIT, function(item, callback) {
+      console.log("Running "+item);
+      currentItem = item;
+      fs.createReadStream('./resources/'+item)
+      .pipe(
+        speech_to_text.createRecognizeStream({
+          content_type: 'audio/l16; rate=44100'
+        }).on('error', function(err){
+          console.log("Speech to Text Stream Error", err);
+          checked[checked.indexOf(item)] = {Status: Completed, fileLocation: "./resources/'+item"};
+          fs.writeFile('./resources/tracker/tracker.txt', checked);
+          fs.writeFile('./results/errors/watson-'+item, err);
+        })
+      ).pipe(
+        fs.createWriteStream('./results/'+item+'.txt').on('error', function(err){
+          console.log("Write Stream Error", err);
+          checked[checked.indexOf(item)] = {Status: Completed, fileLocation: "./resources/'+item"};
+          fs.writeFile('./resources/tracker/tracker.txt', checked);
+          fs.writeFile('./results/errors/write-'+item, err);
+        })
+      ).on('finish', function(){
+        console.log("Conversion finished for "+item);
+        checked[checked.indexOf(item)] = {Status: Completed, fileLocation: "./resources/'+item"};
+        callback(null, true);
+      });
     });
-  });
+  } catch(err) {
+
+  }
 };
